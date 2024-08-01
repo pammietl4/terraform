f137b887522c87716481c47491227b99bce8baa7
Pammi <pammietl4@gmail.com>
2024-08-01 12:18:06 +0530

    list subnets in aregion via cli

--- AWS_TERRAFORM/AWS_CLI_COMMANDS.sh
+++ AWS_TERRAFORM/AWS_CLI_COMMANDS.sh
@@ -3,3 +3,12 @@ aws ec2 describe-instances --region=ap-southeast-1
 
 #list aws ec2 instances in <ap-southeast-1> & copy into a file
 aws ec2 describe-instances --region=ap-southeast-1 >ec2-singapore.sh
+
+#list VPC via aws cli
+aws ec2 describe-vpcs
+
+#list VPC via aws cli in <ap-southeast-1>
+aws ec2 describe-vpcs --region=ap-southeast-1
+
+#list subnets in <ap-southeast-1>
+aws ec2 describe-subnets
