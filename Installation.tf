b392eb44256de515bd89ee2ac7440e83a84662e7
Pammi <pammietl4@gmail.com>
2024-08-01 23:04:35 +0530

    Terraform & aws cli installation

--- Installation.tf
+++ Installation.tf
@@ -1,3 +1,5 @@
+#software installation
+
 # terraform installation guide
 
 step1 -- download the terraform from https://www.terraform.io/ -- (https://releases.hashicorp.com/terraform/1.9.3/terraform_1.9.3_windows_amd64.zip)
@@ -17,3 +19,58 @@ step7 -- select Path variable & Click Edit button -- (it will open a pop up wind
 step8 -- type C:\Tools and click on Add button -- (add the path of terraform.exe file in this step)
 
 step9 -- go to any location & open powershell --- type terraform -- version -- it will show you the version number of your installed terraform software
+
+#validate terraform version from powershell
+terraform --version
+
+#install aws cli software from GUI
+https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+
+#validate aws cli installation
+aws --version
+
+#install phython
+https://www.python.org/downloads/windows/
+
+#install git
+https://git-scm.com/downloads
+
+#validate git installation
+git --version
+
+############ Provide your AWS credentials to backend/aws cli , github credentials to git ################
+
+#setup your aws account details in aws cli
+aws configure
+
+#setup github credentials into git
+git config --global user.name "pammi"
+git config --global user.email "pammietl4@gmail.com"
+git config --global --list
+
+# step1- Establsih connect betwen aws & terraform
+terraform init
+
+# step2 - format the terraform code
+terraform fmt
+
+# step3 - validate the terraform code
+terraform validate
+
+# step4 - plan the terraform code
+terraform plan
+
+# step5 - apply the terraform code
+terraform apply
+
+# step6 - destroy the terraform code
+terraform destroy
+
+
+
+
+
+
+
+
+
