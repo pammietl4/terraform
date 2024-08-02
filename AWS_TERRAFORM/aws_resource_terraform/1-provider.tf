10bd026e18e3628c85cca45180e17441135b2ce2
Pammi <pammietl4@gmail.com>
2024-08-02 12:07:29 +0530

    Added varaible declaration to code

--- AWS_TERRAFORM/aws_resource_terraform/1-provider.tf
+++ AWS_TERRAFORM/aws_resource_terraform/1-provider.tf
@@ -1,6 +1,7 @@
 #crete aws provide in ap-south-1 region
 provider "aws" {
-  region = "ap-south-1"
+  # region = "ap-south-1"
+  region = var.region
 }
 
 
--- AWS_TERRAFORM/aws_resource_terraform/10-variables.tf
+++ AWS_TERRAFORM/aws_resource_terraform/10-variables.tf
@@ -1,17 +1,17 @@
 variable "availability_zone" {}
-variable "key_name" {}
-variable "instance_type" {}
+# variable "instance_type" {}
 variable "region" {}
-variable "ami" {}
-variable "subnet_id" {}
+# variable "ami" {}
+# variable "subnet_id" {}
 variable "vpc_id" {}
-variable "sg_id" {}
-variable "name" {}
-variable "tag" {}
-variable "key" {}
-variable "value" {}
-variable "env" {}
-variable "owner" {}
+# variable "sg_id" {}
+# variable "name" {}
+# variable "tag" {}
+# variable "key" {}
+# variable "key_name" {}
+# variable "value" {}
+# variable "env" {}
+# variable "owner" {}
 variable "cidr_block" {}
 
 
