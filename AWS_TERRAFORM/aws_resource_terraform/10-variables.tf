13f2e5ba93f443204794533d8cfce20955790945
Pammi <pammietl4@gmail.com>
2024-08-02 11:45:13 +0530

    updated the variables in resource.tf

---
+++ AWS_TERRAFORM/aws_resource_terraform/10-variables.tf
@@ -0,0 +1,18 @@
+variable "availability_zone" {}
+variable "key_name" {}
+variable "instance_type" {}
+variable "region" {}
+variable "ami" {}
+variable "subnet_id" {}
+variable "vpc_id" {}
+variable "sg_id" {}
+variable "name" {}
+variable "tag" {}
+variable "key" {}
+variable "value" {}
+variable "env" {}
+variable "owner" {}
+variable "cidr_block" {}
+
+
+
--- AWS_TERRAFORM/aws_resource_terraform/2-resource.tf
+++ AWS_TERRAFORM/aws_resource_terraform/2-resource.tf
@@ -1,6 +1,7 @@
 #creating vpc
 resource "aws_vpc" "genpact-hafeezpet-vpc" {
-  cidr_block = "10.0.0.0/22"
+  # cidr_block = "10.0.0.0/22"
+  cidr_block = var.cidr_block
 
   tags = {
     Name = "genpact-hafeezpet-vpc"
@@ -9,9 +10,11 @@ resource "aws_vpc" "genpact-hafeezpet-vpc" {
 
 #create 1 public subnet
 resource "aws_subnet" "genpact-hafeezpet-public-subnet" {
-  vpc_id            = aws_vpc.genpact-hafeezpet-vpc.id
+  # vpc_id            = aws_vpc.genpact-hafeezpet-vpc.id
+  vpc_id            = var.vpc_id
   cidr_block        = "10.0.0.0/24"
-  availability_zone = "ap-south-1a"
+  # availability_zone = "ap-south-1a"
+  availability_zone = var.availability_zone
 
   tags = {
     Name = "genpact-hafeezpet-public-subnet"
@@ -20,9 +23,10 @@ resource "aws_subnet" "genpact-hafeezpet-public-subnet" {
 
 #create 1 private subnet
 resource "aws_subnet" "genpact-hafeezpet-private-subnet" {
-  vpc_id            = aws_vpc.genpact-hafeezpet-vpc.id
+  # vpc_id            = aws_vpc.genpact-hafeezpet-vpc.id
+  vpc_id            = var.vpc_id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-south-1a"
+  availability_zone = var.availability_zone
 
   tags = {
     Name = "genpact-hafeezpet-private-subnet"
@@ -31,7 +35,8 @@ resource "aws_subnet" "genpact-hafeezpet-private-subnet" {
 
 #create internet gateway
 resource "aws_internet_gateway" "genpact-hafeezpet-igw" {
-  vpc_id = aws_vpc.genpact-hafeezpet-vpc.id
+  # vpc_id = aws_vpc.genpact-hafeezpet-vpc.id
+  vpc_id = var.vpc_id
 
   tags = {
     Name = "genpact-hafeezpet-igw"
@@ -40,7 +45,8 @@ resource "aws_internet_gateway" "genpact-hafeezpet-igw" {
 
 #create route public table
 resource "aws_route_table" "genpact-hafeezpet-public-rt" {
-  vpc_id = aws_vpc.genpact-hafeezpet-vpc.id
+  # vpc_id = aws_vpc.genpact-hafeezpet-vpc.id
+  vpc_id = var.vpc_id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -54,13 +60,15 @@ resource "aws_route_table" "genpact-hafeezpet-public-rt" {
 
 #associate route table with public subnet
 resource "aws_route_table_association" "genpact-hafeezpet-public-rt-association" {
-  subnet_id      = aws_subnet.genpact-hafeezpet-public-subnet.id
+  # subnet_id      = aws_subnet.genpact-hafeezpet-public-subnet.id
+  subnet_id      = var.subnet_id
   route_table_id = aws_route_table.genpact-hafeezpet-public-rt.id
 }
 
 #create private route table
 resource "aws_route_table" "genpact-hafeezpet-private-rt" {
-  vpc_id = aws_vpc.genpact-hafeezpet-vpc.id
+  # vpc_id = aws_vpc.genpact-hafeezpet-vpc.id
+  vpc_id = var.vpc_id
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -81,7 +89,8 @@ resource "aws_route_table_association" "genpact-hafeezpet-private-rt-association
 #create security group
 resource "aws_security_group" "genpact-hafeezpet-sg" {
   name   = "genpact-hafeezpet-sg"
-  vpc_id = aws_vpc.genpact-hafeezpet-vpc.id
+  # vpc_id = aws_vpc.genpact-hafeezpet-vpc.id
+  vpc_id = var.vpc_id
 
   ingress {
     from_port   = 22
