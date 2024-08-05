c5f161f5210ce23295d00dd33d3e1ef38fe57062
Pammi <pammietl4@gmail.com>
2024-08-05 21:54:28 +0530

    Explained about types of storages

---
+++ storage.sh
@@ -0,0 +1,5 @@
+############  AWS STORAGE  ###########
+1. Block storage   -- a) instance storage   b) EBS Volumes
+2. File storage    -- a) AWS EFS (Linux)    b) AWS FSx (Windows)
+3. object storage  -- a) S3 (unstructed data)   b) Glacier (Archival data)
+ 
