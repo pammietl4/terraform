c5c5d87f29e4c89d6079bb42d1885a5cf7e05d36
Pammi <pammietl4@gmail.com>
2024-08-05 10:46:50 +0530

    terraform init types explained

--- terraform_commands.sh
+++ terraform_commands.sh
@@ -4,6 +4,13 @@
 3 -- it will do the provider configuration (downloading the resourece plugins)
 terraform init
 
+# A change in the backend configuration has been detected, which may require migrating existing state.
+# If you wish to attempt automatic migration of the state, use --
+terraform init -migrate-state
+
+# If you wish to store the current configuration with no changes to the state, use --
+terraform init -reconfigure
+
 # step2 - format the terraform code (re-arrange the code Allignment(structure))
 terraform fmt
 
