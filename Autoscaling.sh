b62554aa8a3aeb507c53a6bf26c4c18272106d51
Pammi <pammietl4@gmail.com>
2024-08-14 12:07:29 +0530

    Practicals for Auto scaling

---
+++ Autoscaling.sh
@@ -0,0 +1,16 @@
+Auto-scaling types
+a) Vertical scaling (VPN -- in Kubernetes)
+b) Horizontal scaling (HPA -- in Kubernetes)
+
+Topics required to cover this Auto scaling practicals
+
+1) VPC
+2) Security Group
+3) Target Group
+4) LoadBalancer
+5) Route 53
+6) SNS 
+7) Auto scaling Group
+8) Dyanamic scaling
+9) Alarams
+10) Policies 
